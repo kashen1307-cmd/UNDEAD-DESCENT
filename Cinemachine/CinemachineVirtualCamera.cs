cc59cb25e9257d5ee95a61683fa4261ce41f229e
kashen1307-cmd <kashen1307@gmail.com>
2026-04-05 13:43:09 +0200

    stuff 2

---
+++ Cinemachine/CinemachineVirtualCamera.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Cinemachine
+{
+    public class CinemachineVirtualCamera
+    {
+        internal void OnTargetObjectWarped(Transform transform, Vector3 vector3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
