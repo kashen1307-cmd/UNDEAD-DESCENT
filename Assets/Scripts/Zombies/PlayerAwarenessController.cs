b670bfa8b690c215992094c17e6e73780149cbda
kashen1307-cmd <kashen1307@gmail.com>
2026-04-05 12:38:48 +0200

    Stuff Kivashen
    Player movement script was changed, zombie follows and both player and zombie rotate, also new input manager is used in the settings

---
+++ Assets/Scripts/Zombies/PlayerAwarenessController.cs
@@ -0,0 +1,41 @@
+
+
+using UnityEngine;
+
+public class PlayerAwarenessController : MonoBehaviour
+{
+    public bool AwareOfPlayer { get; private set; }
+
+    public Vector2 DirectionToPlayer { get; private set; }
+
+    [SerializeField]
+    private float playerAwarenessDistance;
+
+    private Transform player;
+
+    private void Awake()
+    {
+        player = Object.FindFirstObjectByType<PlayerMovement>().transform;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 enemyToPLayerVector = player.position - transform.position;
+        DirectionToPlayer = enemyToPLayerVector.normalized;
+
+        if (enemyToPLayerVector.magnitude <= playerAwarenessDistance)
+        {
+            AwareOfPlayer = true;
+        }
+        else
+        {
+            AwareOfPlayer = false;
+        }
+    }
+}
