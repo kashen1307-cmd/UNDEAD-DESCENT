3465d468fb36a0702c1610dcc3b01b3035a7821f
TaiK1505 <taikistnasami@gmail.com>
2026-05-29 03:49:45 +0200

    bullet impact anim

--- Assets/Scripts/Weapons/Bullet/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet/Bullet.cs
@@ -5,6 +5,7 @@ public class Bullet : MonoBehaviour
     public float speed = 10f;
     public Rigidbody2D rb;
     public GameObject Enemy;
+    public GameObject impactPrefab;
 
     private int damage = 1;
 
@@ -30,6 +31,16 @@ public class Bullet : MonoBehaviour
         if (enemy != null)
         {
             enemy.TakeDamage(damage); 
+        }
+
+        if (hitInfo.CompareTag("Wall"))
+        {
+            // Because the bullet is a Trigger, we just tell the right-facing 
+            // spark animation to flip 180 degrees so it bounces backwards!
+            Quaternion bounceRotation = transform.rotation * Quaternion.Euler(0, 0, 0);
+
+            // Spawn the sparks at the bullet's exact location
+            Instantiate(impactPrefab, transform.position, bounceRotation);
         }
 
         Destroy(gameObject);
